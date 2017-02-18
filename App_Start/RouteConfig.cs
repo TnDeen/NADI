e6e979e309f63899bf52546de26c4464e23f60b9
Salehudin <salehuddin@mail.puncaktegap.com.my>
2017-02-18 17:20:43 +0800

    update default route

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVC5
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Student", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                 namespaces: new []{"MVC5.Controllers"}
             );
         }
