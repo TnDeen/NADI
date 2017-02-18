94448fae240c992f9f068135cbd3b888dc9e2ea0
Salehudin <salehuddin@mail.puncaktegap.com.my>
2017-02-18 21:00:59 +0800

    update UI

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC5.Common;
 
 namespace MVC5.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            return View();
+            ViewBag.totalregister = db.Users.Count();
+            return View("Index", "~/Views/Shared/_Layout2.cshtml");
         }
 
         public ActionResult About()
