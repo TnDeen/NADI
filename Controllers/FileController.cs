346555e8436ce9738b8a35ddce903680e4edcc1d
Salehudin <salehuddin@mail.puncaktegap.com.my>
2017-04-18 00:24:17 +0800

    cleanup

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,6 +1,10 @@
 ﻿
 
 using MVC5.Common;
+using OfficeOpenXml;
+using System;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 
 namespace MVC5.Controllers
@@ -19,6 +23,67 @@ namespace MVC5.Controllers
         {
             var fileToRetrieve = db.Files.Find(id);
             return File(fileToRetrieve.Content, fileToRetrieve.ContentType);
+        }
+
+        public FileResult DownloadFile()
+        {
+            string path = HttpContext.Server.MapPath("~/Template/Template/MasterTemplate.xlsx");
+            return File(path, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        }
+
+        public ActionResult Upload(FormCollection formCollection)
+        {
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+
+                    string fileName = file.FileName;
+                    var basePath = Server.MapPath("~/App_Data/uploads");
+                    var path = Path.Combine(basePath, fileName);
+                    if (!Directory.Exists(basePath))
+                    {
+                        Directory.CreateDirectory(basePath);
+                    }
+                    file.SaveAs(path);
+
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+                    
+
+                    using (var package = new ExcelPackage(file.InputStream))
+                    {
+                        var workSheet = package.Workbook.Worksheets["BelianTemplate"];
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                           
+                            var nameraw = workSheet.Cells[rowIterator, 1].Value;
+
+                            if (nameraw == null)
+                            {
+                                break;
+                            }
+                            String nameVal = nameraw.ToString();
+                            String rawdtval = workSheet.Cells[rowIterator, 4].Value.ToString();
+                            String[] dtarray = rawdtval.Split('.');
+                            String kgVal = workSheet.Cells[rowIterator, 2].Value.ToString();
+                            String rmVal = workSheet.Cells[rowIterator, 3].Value.ToString();
+                            String dtVal = dtarray[0] + "/" + dtarray[1] + "/" + dtarray[2];
+
+                           
+                           
+                          
+                        }
+                    }
+
+                }
+            }
+            return View();
         }
     }
 }
---
+++ Migrations/201704171622308_cleanup.cs
@@ -0,0 +1,75 @@
+namespace MVC5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class cleanup : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Enrollments", "Student_Id", "dbo.Students");
+            DropForeignKey("dbo.Enrollments", "CourseId", "dbo.Courses");
+            DropIndex("dbo.Enrollments", new[] { "CourseId" });
+            DropIndex("dbo.Enrollments", new[] { "Student_Id" });
+            DropTable("dbo.Courses");
+            DropTable("dbo.Enrollments");
+            DropTable("dbo.Students");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        DateCreated = c.DateTime(nullable: false),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(),
+                        DateUpdated = c.DateTime(nullable: false),
+                        LastUpdated = c.DateTime(),
+                        LastUpdatedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Enrollments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CourseId = c.Int(nullable: false),
+                        Grade = c.Decimal(precision: 18, scale: 2),
+                        DateCreated = c.DateTime(nullable: false),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(),
+                        DateUpdated = c.DateTime(nullable: false),
+                        LastUpdated = c.DateTime(),
+                        LastUpdatedBy = c.String(),
+                        Student_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CourseName = c.String(),
+                        Credit = c.Int(nullable: false),
+                        DateCreated = c.DateTime(nullable: false),
+                        CreateDate = c.DateTime(),
+                        CreateBy = c.String(),
+                        DateUpdated = c.DateTime(nullable: false),
+                        LastUpdated = c.DateTime(),
+                        LastUpdatedBy = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateIndex("dbo.Enrollments", "Student_Id");
+            CreateIndex("dbo.Enrollments", "CourseId");
+            AddForeignKey("dbo.Enrollments", "CourseId", "dbo.Courses", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.Enrollments", "Student_Id", "dbo.Students", "Id");
+        }
+    }
+}
